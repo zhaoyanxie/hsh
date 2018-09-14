881e22308c993db436d4f09bb705b1f87464b705
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-09-14 18:07:59 +0800

    Removed /hsh in endpoints

--- src/pages/endpoints.js
+++ src/pages/endpoints.js
@@ -1,6 +1,6 @@
-export const HOMEPAGE = "/hsh";
-export const OUR_PRODUCTS = "/hsh/our_products";
-export const OUR_PRODUCTS_ADD = "/hsh/our_products/add";
-export const OUR_STORES = "/hsh/our_stores";
-export const CONTACT = "/hsh/contact_us";
-export const RFQ = "/hsh/rfq";
+export const HOMEPAGE = "/";
+export const OUR_PRODUCTS = "/our_products";
+export const OUR_PRODUCTS_ADD = "/our_products/add";
+export const OUR_STORES = "/our_stores";
+export const CONTACT = "/contact_us";
+export const RFQ = "/rfq";
