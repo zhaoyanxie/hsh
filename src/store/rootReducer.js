5d697d24bea9e02afca049425d7c398f7926a886
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-09-20 12:12:19 +0800

    Refactored and added reduce rfq item quantity

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,50 +1,64 @@
-import { ADD_RFQ_ITEM, REMOVE_RFQ_ITEM } from "./types";
+import { ADD_RFQ_ITEM, REMOVE_RFQ_ITEM, REDUCE_RFQ_ITEM } from "./types";
 
 const initialState = {
   rfqItems: [],
   id: 0
 };
 
+const findRfqItem = (state, action) => {
+  return state.rfqItems.filter(item => item.productId === action.productId);
+};
+
+const addFirstRfqItem = (newState, action) => {
+  return {
+    ...newState,
+    rfqItems: [
+      ...newState.rfqItems,
+      {
+        productId: action.productId,
+        description: action.description,
+        code: action.code,
+        minQty: action.minQty,
+        uom: action.uom,
+        qty: 1
+      }
+    ]
+  };
+};
+
+const changeRfqQty = (newState, action, newRfqItem, newQty) => {
+  const newRfqItemIndex = newState.rfqItems.findIndex(
+    item => item.productId === action.productId
+  );
+  return {
+    ...newState,
+    rfqItems: [
+      ...newState.rfqItems.slice(0, newRfqItemIndex),
+      { ...newRfqItem[0], qty: newQty },
+      ...newState.rfqItems.slice(newRfqItemIndex + 1)
+    ]
+  };
+};
+
 const rootReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  const newRfqItem = findRfqItem(state, action);
+
   switch (action.type) {
     case ADD_RFQ_ITEM:
-      const newState = { ...state };
-      const newRfqItem = state.rfqItems.filter(
-        item => item.productId === action.productId
-      );
       newState.id++;
-
       if (newRfqItem.length === 0) {
         // if the new rfqItem to be added is not in the current list of rfqItems
-        return {
-          ...newState,
-          rfqItems: [
-            ...newState.rfqItems,
-            {
-              productId: action.productId,
-              description: action.description,
-              code: action.code,
-              minQty: action.minQty,
-              uom: action.uom,
-              qty: 1
-            }
-          ]
-        };
+        return addFirstRfqItem(newState, action);
       } else {
         // new rfqItem exists and only its qty is to be increased
-        const newRfqItemIndex = state.rfqItems.findIndex(
-          item => item.productId === action.productId
-        );
         const newQty = newRfqItem[0].qty + 1;
-        return {
-          ...newState,
-          rfqItems: [
-            ...newState.rfqItems.slice(0, newRfqItemIndex),
-            { ...newRfqItem[0], qty: newQty },
-            ...newState.rfqItems.slice(newRfqItemIndex + 1)
-          ]
-        };
+        return changeRfqQty(newState, action, newRfqItem, newQty);
       }
+    case REDUCE_RFQ_ITEM:
+      newState.id++;
+      const newQty = Math.max(newRfqItem[0].qty - 1, 0);
+      return changeRfqQty(newState, action, newRfqItem, newQty);
     case REMOVE_RFQ_ITEM:
       const rfqItems = state.rfqItems.filter(
         item => item.productId !== action.productId
--- src/store/types.js
+++ src/store/types.js
@@ -1,2 +1,3 @@
 export const ADD_RFQ_ITEM = "ADD_RFQ_ITEM";
+export const REDUCE_RFQ_ITEM = "REDUCE_RFQ_ITEM";
 export const REMOVE_RFQ_ITEM = "REMOVE_RFQ_ITEM";
