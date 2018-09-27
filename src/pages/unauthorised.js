d714591f30aa01f47133c30364e531681e91dc22
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-09-27 21:39:17 +0800

    Added unauthorised page

---
+++ src/pages/unauthorised.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Unauthorised = () => (
+  <div>
+    <h1>Unauthorised</h1>
+    <p>You do not have the permission to access this page.</p>
+  </div>
+);
+
+export default Unauthorised;
